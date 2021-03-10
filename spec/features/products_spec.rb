4c9aaca7f7b95d7afd13254e8d5f25fc17951527
JiaMingXie <jiamingxie@JiaMingXiedeMacBook-Pro.local>
2021-03-10 16:30:34 +0800

    product list ordered on created_at by descent

--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.feature "Products", type: :feature do
+  describe '#index order' do 
+    it 'ordered by descent' do 
+      visit root_path
 
+      loaded_content = all('.row > .name').map(&:text)
+
+      expect(Product.order(created_at: :desc).pluck(:name)).to eq(loaded_content)      
+    end
+  end
 end
