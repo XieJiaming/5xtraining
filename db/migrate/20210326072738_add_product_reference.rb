f894269c7a0da379a4413f00bfdbeb3a45c4c357
JiaMingXie <dack1227@gmail.com>
2021-04-01 19:21:33 +0800

    fix migrate

--- db/migrate/20210326072738_add_product_reference.rb
+++
@@ -1,5 +0,0 @@
-class AddProductReference < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :products, :users
-  end
-end
--- db/migrate/20210326083036_remove_product_ref.rb
+++
@@ -1,5 +0,0 @@
-class RemoveProductRef < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :products, :users_id
-  end
-end
